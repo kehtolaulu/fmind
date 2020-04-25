b064032a29acbdac453914b8d44a4d0b5e4c8b5f
Dmitry Barskov <barskoidim@gmail.com>
2020-04-25 22:06:26 +0300

    Rich article view

--- lib/ui/article_view_widget.dart
+++ lib/ui/article_view_widget.dart
@@ -15,7 +15,23 @@ class ArticleView extends StatelessWidget {
   const ArticleView(this.article);
 
   @override
-  Widget build(BuildContext context) {
-    return SimpleWrapper(article.title, Text(article.content));
-  }
+  Widget build(BuildContext context) => SimpleWrapper(
+      article.title,
+      Column(
+        children: alignLeft([title, Divider(), content]),
+      ));
+
+  List<Widget> alignLeft(List<Widget> widgets) => widgets
+      .map((w) => Align(alignment: Alignment.centerLeft, child: w))
+      .toList();
+
+  Text get title => Text(article.title, style: titleStyle);
+
+  TextStyle get titleStyle =>
+      TextStyle(fontWeight: FontWeight.w600, fontSize: 36.0);
+
+  Text get content => Text(article.content, style: contentStyle);
+
+  TextStyle get contentStyle =>
+      TextStyle(fontSize: 24.0, fontWeight: FontWeight.w300);
 }
