411333a86192198b6fd4ff5d7536cbea4728395c
Dmitry Barskov <barskoidim@gmail.com>
2020-04-25 22:38:07 +0300

    Refactor ArticleRreview + fix error when tapping article

--- lib/ui/article_preview_widget.dart
+++ lib/ui/article_preview_widget.dart
@@ -9,42 +9,39 @@ class ArticlePreview extends StatelessWidget {
   ArticlePreview(this.article);
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            preview(context),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[readTime, star],
-              ),
-            ),
-          ],
-        ),
-        Divider(height: 2.0, color: Colors.grey)
-      ],
-    );
-  }
+  Widget build(BuildContext context) => Column(
+        children: [
+          articleRow(context),
+          Divider(height: 2.0, color: Colors.grey)
+        ],
+      );
 
-  Widget preview(BuildContext context) => new GestureDetector(
-        onTap: () => ArticleView.navigate(context, article),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[title, contentPreview],
-        ),
+  Widget articleRow(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+              onTap: () => ArticleView.navigate(context, article),
+              behavior: HitTestBehavior.translucent,
+              child: preview),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [readTime, star],
+            ),
+          ),
+        ],
+      );
+
+  Widget get preview => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [title, contentPreview],
       );
 
   Widget get title => Padding(
-        padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 6.0),
-        child: Text(
-          article.title,
-          style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-        ),
-      );
+      padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 6.0),
+      child: Text(article.title,
+          style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold)));
 
   Widget get contentPreview => Padding(
         padding: const EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 12.0),
@@ -60,7 +57,7 @@ class ArticlePreview extends StatelessWidget {
       );
 
   Widget get star => Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(8.0),
         child: Icon(
           Icons.star_border,
           size: 35.0,
