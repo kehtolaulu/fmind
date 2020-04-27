d9f993f7a606b0287fcf89f979c2b090f52e93e8
Dmitry Barskov <barskoidim@gmail.com>
2020-04-27 18:38:46 +0300

    add tasks examples

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -9,14 +9,19 @@ import 'articles.dart';
 class Home extends StatelessWidget {
   final List<Article> articles = [
     Article("How to meet ladies", [
-      Part("Intro", "How to meet ladies, how to interest them...",)
+      Part("Intro", "How to meet ladies, how to interest them...",
+          [Practice.withContent("Talk to a woman.")])
     ]),
     Article("How to pump your body", [
-      Part("Intro", "How to perform 5 minute training and to get...",),
-      Part("Next level", "Blah bla bla",)
+      Part("Intro", "How to perform 5 minute training and to get...",
+          [Practice.withContent("Do 20 push ups.")]
+          ),
+      Part("Next level", "Blah bla bla",
+          [Practice.withContent("Do 20 pull ups.")])
     ]),
     Article("How to get energy", [
-      Part("Abstract", "How to gain energy from our natural stock...",)
+      Part("Abstract", "How to gain energy from our natural stock...",
+          [CheckedQuestion.yesNo("Are you tired?", "Yes")])
     ])
   ];
 
