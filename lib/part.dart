4e870a98b9fa36809c8eb58fabc6e10d6f2a9741
Dmitry Barskov <barskoidim@gmail.com>
2020-04-27 18:16:31 +0300

    add part model

---
+++ lib/part.dart
@@ -0,0 +1,10 @@
+import 'tasks/task.dart';
+
+/// A part of an article
+/// contains some text and task after
+class Part {
+  String title;
+  String content;
+
+  Part(this.title, this.content);
+}
