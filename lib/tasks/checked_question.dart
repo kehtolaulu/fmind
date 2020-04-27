bb430911d54dc2335b6decc53d93dfaa9ebbec2e
Dmitry Barskov <barskoidim@gmail.com>
2020-04-27 20:17:53 +0300

    add present() method to tasks

--- lib/tasks/checked_question.dart
+++ lib/tasks/checked_question.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:fmind/tasks/task.dart';
+import 'package:fmind/ui/tasks_views/checked_question_view.dart';
 
 /// Question that has only one correct answer
 class CheckedQuestion implements Task {
@@ -9,4 +11,7 @@ class CheckedQuestion implements Task {
   CheckedQuestion(this.content, this.answers, this.correctAnswer);
 
   CheckedQuestion.yesNo(this.content, this.correctAnswer) : answers = ["Yes", "No"];
+
+  @override
+  Widget present() => CheckedQuestionView(this);
 }
--- lib/tasks/practice.dart
+++ lib/tasks/practice.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:fmind/tasks/task.dart';
+import 'package:fmind/ui/tasks_views/practice_view.dart';
 
 /// Task that requires user to do something
 class Practice implements Task {
@@ -8,4 +10,7 @@ class Practice implements Task {
   Practice(this.content, this.done);
 
   Practice.withContent(this.content);
+
+  @override
+  Widget present() => PracticeView(this);
 }
